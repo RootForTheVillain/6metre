67e1b6bf2ba59ac6d51b43c1e90e41fa83b4df2b
Brent Knop <Brent Knop>
2017-04-23 15:59:59 -0400

    Layout complete, still buggy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $('nav').addClass("navbar-shadow");
+    }
+    else {
+        $('nav').removeClass("navbar-shadow");
+    }
+});
+
 $(document).ready(function() {
     $('#copyright-date').text(new Date().getFullYear());
-    console.log($('#copyright-date'), new Date().getFullYear());
 });
