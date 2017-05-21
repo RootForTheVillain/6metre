dde88bd6117e7193fa30c0e526238d4fa99e2b6e
Brent Knop <Brent Knop>
2017-05-21 13:25:15 -0400

    A lot of stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,53 @@ $(window).scroll(function() {
 $(document).ready(function() {
     $('#copyright-date').text(new Date().getFullYear());
 
-    $.each($('[data-include]'), function() {
-      $(this).load('assets/includes/' + $(this).data('include') + '.html');
-    });
-
-    /*if (window.location.href.indexOf('?') > -1) {
-        $('#background-container').css('background-image', "url('assets/images/background - Copy.png')");
-        console.log('god dammit')
-    }*/
+    loadPartial(window.location.hash);
+    bindRouterLinks();
 });
+
+function bindRouterLinks() {
+  $('.router-link').off('click').click(function(e) {
+    e.stopPropagation();
+    loadPartial($(this).attr('href'));
+  });
+}
+
+function loadPartial(href) {
+  href = href.toLowerCase();
+  switch (href) {
+    case '#about-6metre-financial':
+    case '#360-degree-planning':
+    case '#products-and-services':
+    case '#contact':
+      href = href.replace('#', '');
+      $('body').removeAttr('id');
+    break;
+    default:
+      href = 'home';
+      $('body').attr('id', 'splash');
+    break;
+  }
+
+  $('[data-include]').load(href + '.html', function() {
+    bindRouterLinks();
+    initPage(href);
+  });
+}
+
+function initPage(href) {
+  switch (href.toLowerCase()) {
+    case 'about-6metre-financial':
+
+      var today = new Date();
+      $('.financial-advisor-experience').text(dateDiff(today, new Date('4/1/2008')));
+      $('.lending-experience').text(dateDiff(today, new Date('4/1/2015')));
+      $('.training-experience').text(dateDiff(today, new Date('4/1/2012')));
+
+    break;
+  }
+}
+
+function dateDiff(date1, date2) {
+  var msInAYear = 31536000000;
+  return Math.floor(Math.abs(date1 - date2) / msInAYear);
+}
